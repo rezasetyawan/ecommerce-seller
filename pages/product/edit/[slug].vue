adf122a3aae16499e218c9474bcb666991924621
Reza Setyawan <104214899+rezasetyawan@users.noreply.github.com>
2023-11-22 14:56:05 +0700

    Develop (#4)
    * hotfix: preview image while editing product

--- pages/product/edit/[slug].vue
+++ pages/product/edit/[slug].vue
@@ -310,7 +310,7 @@ definePageMeta({
             <Textarea name="product_description" v-model="product.description" required />
 
             <template v-for="(image, index) in images" :key="index">
-                 v-if="image" :src="getImageUrl(image)" alt="Selected Image" class="max-w-[150px] mt-3 shadow-md" />
+                <img v-if="image" :src="getImageUrl(image)" alt="Selected Image" class="max-w-[150px] mt-3 shadow-md" />
             </template>
 
 
